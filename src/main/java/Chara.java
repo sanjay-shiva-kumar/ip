2d3675eb516bd61c845799b3087adec02515ecf8
SANJAYS_LAPTOP\sanja <sanjayshivakumarfsl@gmail.com>
2025-08-27 12:51:37 +0800

    Add echo functionality, ASCII art

--- src/main/java/Chara.java
+++ src/main/java/Chara.java
@@ -1,12 +1,8 @@
-    public class Chara {
-    /*public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-    }*/
+import java.util.Scanner;
+
+public class Chara {
+    public static long lineLen = 50;
+
     public static void printLine(long length) {
         for (long i = 0; i < length; i+=1) {
             System.out.print('_');
@@ -14,13 +10,37 @@
         System.out.println();
     }
 
-    public static void main(String[] args) {
-        long lineLen = 50;
+    public static void initialPrint() {
+        printLine(lineLen);
+        String logo = """
+                  ____ _   _    _    ____   _   \s
+                 / ___| | | |  / \\  |  _ \\ / \\  \s
+                | |   | |_| | / _ \\ | |_) / _ \\ \s
+                | |___|  _  |/ ___ \\|  _ < ___ \\\s
+                 \\____|_| |_/_/   \\_\\_| \\_\\_/ \\_\\
+                """;
+        System.out.print(logo);
         printLine(lineLen);
         System.out.println("Hello! I'm Chara!");
         System.out.println("What can I do for you?");
         printLine(lineLen);
-        System.out.println(" Bye. Hope to see you again soon!");
-        printLine(lineLen);
+    }
+    public static void main(String[] args) {
+        initialPrint();
+        Scanner echo = new Scanner(System.in);
+        while (true) {
+            System.out.print("User: ");
+
+            String userIn = echo.nextLine();
+            if (userIn.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                printLine(lineLen);
+                return;
+            } else {
+                System.out.println(userIn);
+                printLine(lineLen);
+            }
+        }
+
     }
 }
